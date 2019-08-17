4be0e5a7f68d29d59c3135e9ad9224f515558bf9
Nikolay Dema <ndema2301@gmail.com>
2019-08-18 02:08:08 +0300

    normals

--- simple_spheres/adding_sphere/rend.cpp
+++ simple_spheres/adding_sphere/rend.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
 #include "ray.h"
 
-bool hit_sphere(const vec3& sphere_center, float radius, const ray& r) {
+float hit_sphere(const vec3& sphere_center, float radius, const ray& r) {
     vec3 oc = r.origin() - sphere_center;
     float a = dot(r.direction(), r.direction());
     float b = 2.0 * dot(oc, r.direction());
     float c = dot(oc, oc) - radius*radius;
     float discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
+    if (discriminant < 0) {
+        return (-1.0);
+    }
+    else {
+        return (-b - sqrt(discriminant)) / (2.0*a);
+    }
 }
 
 vec3 color(const ray& r) {
-    if (hit_sphere(vec3(-0.6, 0,-1), 0.2, r))
-        return vec3(0.3, 0, 0);
-    vec3 unit_direction = make_unit(r.direction());
-    float t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.6, 0.8, 1.0);
+    vec3 new_sphere = vec3(0, 0, -1);
+    float t = (float)hit_sphere(new_sphere, 0.2, r);
+    if (t > 0) {
+        vec3 N = make_unit(r.point_at_param(t) - new_sphere);
+        return 0.5*vec3(N.x()+1, N.y()+1, N.z()+1);
+    }
+    else {
+        vec3 unit_direction = make_unit(r.direction());
+        t = 0.5*(unit_direction.y() + 1.0);
+        return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.6, 0.8, 1.0);
+    }
 }
 
 int main () {
-   int nx = 2000;
-   int ny = 1000;
+   int nx = 1200;
+   int ny = 600;
    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
    vec3 lower_left_corner(-2.0, -1.0, -2.0);
