00425ffab37a981b74c11f39e2d84a72a29c0e65
Nikolay Dema <ndema2301@gmail.com>
2019-08-25 06:17:58 +0300

    hitable_list ok

--- simple_spheres/adding_sphere/rend.cpp
+++ simple_spheres/adding_sphere/rend.cpp
@@ -1,31 +1,20 @@
 #include <iostream>
-#include "ray.h"
+#include "sphere.h"
+#include "hitable_list.h"
+#include "float.h"
 
-float hit_sphere(const vec3& sphere_center, float radius, const ray& r) {
-    vec3 oc = r.origin() - sphere_center;
-    float a = dot(r.direction(), r.direction());
-    float b = 2.0 * dot(oc, r.direction());
-    float c = dot(oc, oc) - radius*radius;
-    float discriminant = b*b - 4*a*c;
-    if (discriminant < 0) {
-        return (-1.0);
-    }
-    else {
-        return (-b - sqrt(discriminant)) / (2.0*a);
-    }
-}
 
-vec3 color(const ray& r) {
-    vec3 new_sphere = vec3(0, 0, -1);
-    float t = (float)hit_sphere(new_sphere, 0.2, r);
-    if (t > 0) {
-        vec3 N = make_unit(r.point_at_param(t) - new_sphere);
-        return 0.5*vec3(N.x()+1, N.y()+1, N.z()+1);
+vec3 color(const ray& r, hitable *world) {
+    hit_record rec;
+    // vec3 p = r.point_at_param(1.0);
+    // std::cout << "p: " << p << "\n";
+    if (world->hit(r, 0.0, 2.0, rec)) {
+        return 0.5*vec3(rec.n.x()+1, rec.n.y()+1, rec.n.z()+1);
     }
     else {
         vec3 unit_direction = make_unit(r.direction());
-        t = 0.5*(unit_direction.y() + 1.0);
-        return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.6, 0.8, 1.0);
+        float t = 0.5*(unit_direction.y() + 1.0);
+        return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 0.9);
     }
 }
 
@@ -39,12 +28,19 @@ int main () {
    vec3 vertical(0.0, 2.0, 0.0);
    vec3 origin(0.0, 0.0, 0.0);
 
+   hitable *list[2];
+   list[0] = new sphere(vec3(0, 0, -1), 0.2);
+   list[1] = new sphere(vec3(0, -2.08, -1), 2);
+   hitable *world = new hitable_list(list, 2);
+
    for (int j = ny-1; j >= 0; j--) {
       for (int i = 0; i < nx; i++) {
          float u = float(i) / float(nx);
          float v = float(j) / float(ny);
          ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-         vec3 col = color(r);
+         // vec3 p = r.point_at_param(1.0);
+         // std::cout << "p: " << p << "\n";
+         vec3 col = color(r, world);
          int r_int = int(240*col.r());
          int g_int = int(240*col.g());
          int b_int = int(240*col.b());
--- simple_spheres/include/sphere.h
+++ simple_spheres/include/sphere.h
@@ -13,27 +13,39 @@ class sphere: public hitable {
 };
 
 bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+    // std::cout << "in shpere hit" << "\n";
     vec3 oc = r.origin() - center;
     float a = dot(r.direction(), r.direction());
     float b = dot(oc, r.direction());
     float c = dot(oc, oc) - radius*radius;
-    float d = b*b - a*c;
-    if (d > 0) {
-        float temp = (-b - sqrt(d))/a;
+    float discriminant = b*b - a*c;
+    // std::cout << "dis: " << discriminant << "\n";
+    if (discriminant > 0.0) {
+        float temp = (-b - sqrt(discriminant))/a;
+        // std::cout << "temp: " << temp << "\n";
         if (temp < t_max && t_min < temp) {
             rec.t = temp;
             rec.p = r.point_at_param(temp);
             rec.n = (rec.p - center)/radius;
+            // std::cout << "-b-" << "\n";
             return true;
         }
-        float temp = (-b + sqrt(d))/a;
+        // else {
+        //     std::cout << "no b- | temp :"<< temp << "\n";
+        // }
+        temp = (-b + sqrt(discriminant))/a;
         if (temp < t_max && t_min < temp) {
             rec.t = temp;
             rec.p = r.point_at_param(temp);
             rec.n = (rec.p - center)/radius;
+            // std::cout << "-b+" << "\n";
             return true;
         }
+        // else {
+        //     std::cout << temp << "\n";
+        // }
     }
+    // std::cout << "f" << "\n";
     return false;
 }
 
